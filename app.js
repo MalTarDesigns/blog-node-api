628570fa24e411af119f54f96d6c4430b8cd64cc
Jamal Johnson <jamal.johnson@pentius.com>
2019-03-31 13:55:23 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const config = require('./config.json');
+
+const postsRoutes = require("./routes/posts");
+
+const app = express();
+
+mongoose
+  .connect(
+    config.development.database,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(err => {
+    console.log("Connection failed!", err);
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/images", express.static(path.join("backend/images"))); // any request is allowed from /images
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello Digital Ocean!')
+})
+
+app.use("/api/posts", postsRoutes);
+
+module.exports = app;
